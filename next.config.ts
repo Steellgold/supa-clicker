ad63d305a18034d7b8ee1e35c9fb2b5f48a57793
Gaëtan H <gaetanhus@gmail.com>
2025-07-19 17:28:24 +0200

    Update Next.js configuration for image remote patterns and add Radix UI Scroll Area dependency

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL("https://s7yh4pytyr.ufs.sh/f/**")],
+  }
 };
 
 export default nextConfig;
